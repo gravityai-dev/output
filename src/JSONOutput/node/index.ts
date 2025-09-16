76932049dab3519a8e28fd435d16a281e187e11e
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 20:26:10 +0400

    v1.0.14

--- src/JSONOutput/node/index.ts
+++ src/JSONOutput/node/index.ts
@@ -8,7 +8,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.12",
+    packageVersion: "1.0.13",
     type: NODE_TYPE,
     isService: false,
     name: "JSON Output",
